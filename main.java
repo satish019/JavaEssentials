dd7c382056428d75aae621c3f03557bf4ce2d69f
Satish <bokkasatish019@gmail.com>
2020-10-17 03:28:29 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+package com.QuizApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Quiz application
+        Game game=new Game();
+        game.initGame();
+        game.play();
+    }
+}
